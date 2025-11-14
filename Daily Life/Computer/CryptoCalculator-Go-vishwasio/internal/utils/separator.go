5b752088512d9e024c3b2f8de1d710e18016bbe7
vishwasio <im.vishwas.03@gmail.com>
2025-11-14 16:59:49 +0530

    feat: add input helper and UI utility components
    - Implemented input validation helper (Prompt)
    - Added command-line separator utility

---
+++ Daily Life/Computer/CryptoCalculator-Go-vishwasio/internal/utils/separator.go
@@ -0,0 +1,9 @@
+// Author: Vishwas Karode (GitHub: vishwasio)
+
+package utils
+
+import "fmt"
+
+func Separator() {
+    fmt.Println("------------------------------------------------------------")
+}
