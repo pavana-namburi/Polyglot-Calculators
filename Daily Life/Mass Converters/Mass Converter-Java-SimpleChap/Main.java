2ffecd01af181ceefcb950c2a665c344c449fdd8
SimpleChap <patelmit1129@gmail.com>
2025-11-16 08:38:03 -0600

    "Added more units to convert"

--- Daily Life/Mass Converters/Mass Converter-Java-SimpleChap/Main.java
+++ Daily Life/Mass Converters/Mass Converter-Java-SimpleChap/Main.java
@@ -8,10 +8,33 @@ public class Main {
     private static final Map<String, Double> TO_GRAMS = new HashMap<>();
 
     static {
+        TO_GRAMS.put("microgram", 0.000001);
+        TO_GRAMS.put("µg", 0.000001);
+        TO_GRAMS.put("ug", 0.000001);
+
+        TO_GRAMS.put("milligram", 0.001);
+        TO_GRAMS.put("mg", 0.001);
+
         TO_GRAMS.put("gram", 1.0);
+        TO_GRAMS.put("g", 1.0);
+
         TO_GRAMS.put("kilogram", 1000.0);
+        TO_GRAMS.put("kg", 1000.0);
+
+        TO_GRAMS.put("ton", 1_000_000.0);
+        TO_GRAMS.put("t", 1_000_000.0);
+
         TO_GRAMS.put("pound", 453.59237);
+        TO_GRAMS.put("lb", 453.59237);
+
         TO_GRAMS.put("ounce", 28.3495231);
+        TO_GRAMS.put("oz", 28.3495231);
+
+        TO_GRAMS.put("stone", 6350.29318);
+        TO_GRAMS.put("st", 6350.29318);
+
+        TO_GRAMS.put("carat", 0.2);
+        TO_GRAMS.put("ct", 0.2);
     }
 
     /**
@@ -26,7 +49,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=== Mass Converter ===");
-        System.out.println("Supported units: gram, kilogram, pound, ounce\n");
+        System.out.println("Supported units:");
+        System.out.println("  microgram  (µg, ug)");
+        System.out.println("  milligram  (mg)");
+        System.out.println("  gram       (g)");
+        System.out.println("  kilogram   (kg)");
+        System.out.println("  ton        (t)");
+        System.out.println("  pound      (lb)");
+        System.out.println("  ounce      (oz)");
+        System.out.println("  stone      (st)");
+        System.out.println("  carat      (ct)");
+        System.out.println();
 
         System.out.print("Enter value: ");
         double value = scanner.nextDouble();
